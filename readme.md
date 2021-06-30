05ce12f384ba7f7bfb4e6d4db195af001f8a43d9
Vit Listik <tivvit@seznam.cz>
2021-06-30 22:34:13 +0200

    generate only valid pairs

--- readme.md
+++ readme.md
@@ -1,7 +1,13 @@
 ### What is the average distance of randomly distributed points within a square?
 
-#### v1
+#### 1.0.0
 ```
 average distance is 0.6699530422371169
 python3 simulate.py  147.59s user 2.23s system 99% cpu 2:31.24 total
+```
+
+#### 1.1.0
+```
+average distance is 0.6691942634085619
+python3 simulate.py  62.94s user 0.95s system 99% cpu 1:04.40 total
 ```
--- simulate.py
+++ simulate.py
@@ -8,8 +8,14 @@ def dist(points):
     return (abs(x[0] - y[0]) ** 2 + abs(x[1] - y[1])) ** (1 / 2)
 
 
+def pairs_without_rep(points):
+    for i in range(len(points)):
+        for j in range(i, len(points)):
+            yield points[i], points[j]
+
+
 if __name__ == '__main__':
     SAMPLES = 10000
     points = [(random(), random()) for _ in range(SAMPLES)]
-    distances = map(dist, permutations(points, 2))
+    distances = map(dist, pairs_without_rep(points))
     print(f"average distance is {mean(distances)}")
